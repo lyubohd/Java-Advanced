1640b3b88d59024ed46ea9c2a86d6338af3bd439
Lyubomir Tomanov <lyubomir.tomanov@gmail.com>
2021-01-25 19:42:45 +0200

    Added first problem

--- src/streamsFilesAndDirectoriesExercises/P01_SumLines.java
+++ src/streamsFilesAndDirectoriesExercises/P01_SumLines.java
@@ -1,7 +1,26 @@
 package streamsFilesAndDirectoriesExercises;
 
-public class P01_SumLines {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class P01_SumLines {
+    private static final String PATH = "src/streamsFilesAndDirectoriesExercises/resources/input.txt";
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(PATH))) {
+
+            String line = reader.readLine();
+            while (line != null) {
+                int sum = 0;
+                for (int i = 0; i < line.length(); i++) {
+                    sum += line.charAt(i);
+                }
+
+                System.out.println(sum);
+                line = reader.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
