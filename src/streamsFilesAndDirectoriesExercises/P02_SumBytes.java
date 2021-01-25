0c66ec56c1389738a6de868deb7ac8dcc0b3612d
Lyubomir Tomanov <lyubomir.tomanov@gmail.com>
2021-01-25 19:55:04 +0200

    Added second problem from Streams, Files and Directories - Exercises

---
+++ src/streamsFilesAndDirectoriesExercises/P02_SumBytes.java
@@ -0,0 +1,27 @@
+package streamsFilesAndDirectoriesExercises;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class P02_SumBytes {
+    private static final String PATH = "src/streamsFilesAndDirectoriesExercises/resources/input.txt";
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(PATH))) {
+
+            String line = reader.readLine();
+            int sum = 0;
+            while (line != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    sum += line.charAt(i);
+                }
+
+                line = reader.readLine();
+            }
+            System.out.println(sum);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
